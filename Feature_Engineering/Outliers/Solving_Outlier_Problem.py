cac0c4e7bfb67b8ff001cee7a1f03df59889e052
ARTI-c700 <amiljamilov01@gmail.com>
2024-03-19 13:33:01 +0400

    Applying a functions/procedures for solving outlier dilemma (modified)

--- Feature_Engineering/Outliers/Solving_Outlier_Problem.py
+++ Feature_Engineering/Outliers/Solving_Outlier_Problem.py
@@ -4,9 +4,9 @@ from Feature_Engineering.Outliers.Getting_Outlier_Values import *
 from Feature_Engineering.Outliers.Acquisition_of_Functioning_2 import *
 from Feature_Engineering.Required_Packages import *
 
-#####################################
+##########################################################################
 # Removing
-
+##########################################################################
 low, up = outlier_thresholds(load_titanic(), "Fare")
 df = load_titanic()
 
@@ -26,6 +26,24 @@ shape = df[~((df["Fare"] < low) | (df["Fare"] > up))].shape
 def remove_outlier(dataframe, column_name):
     low_limit, up_limit = outlier_thresholds(dataframe, column_name)
     return dataframe[~((dataframe[column_name] < low_limit) | (dataframe[column_name] > up_limit))]
+
+
+def remove_outliers(data_frame):
+    (categoric, numeric,
+     cat_in_numeric, cardinal_in_cat) = grab_col_names(data_frame)
+    df_cpy = data_frame
+
+    numeric = [smp_num for smp_num in numeric if smp_num != "PassengerId"]
+
+    thresholds = {}
+    for column in numeric:
+        thresholds[column] = outlier_thresholds(data_frame, column)
+
+    for column in numeric:
+        _low, _up = thresholds[column]
+        df_cpy = df_cpy[~((df_cpy[column] < _low) | (df_cpy[column] > _up))]
+
+    return df_cpy
 
 
 cat, num, cat_in_num, car_in_cat = grab_col_names(df)
@@ -48,6 +66,7 @@ for col in num:
 # In the cleaning (or removing) process each variable can require the cleaning of different rows
 # As a result, there should be used an integration of all results based on their variables
 
+"""SOLUTION 1 - DRAFT"""
 """ Declaring DataFrame using Lists (required)
 import pandas as pd
  
@@ -61,8 +80,7 @@ df = pd.DataFrame(data, columns=['Name', 'Age'])
 print(df)
 
 """
-
-""" Declaring DataFrame using
+""" Declaring DataFrame using (DRAFT)
 
 def generate_dataframe(dataframe, cols):
     iframes = []
@@ -106,3 +124,39 @@ for el in i2frames:
         print("-------------------------------------")
 
 """
+"""-----"""
+
+"""SOLUTION - 2"""
+new_one = remove_outliers(df)
+ll1, uu1 = outlier_thresholds(df, "Age")
+ll2, uu2 = outlier_thresholds(df, "Fare")
+
+print(new_one[(new_one["Age"] < ll1) | (new_one["Age"] > uu1)])
+print(new_one[(new_one["Fare"] < ll2) | (new_one["Fare"] > uu2)])
+
+
+##########################################################################
+# Re-assignment with thresholds
+##########################################################################
+
+def replace_with_thresholds(dataframe):
+    (categoric, numeric,
+     cat_in_numeric, cardinal_in_cat) = grab_col_names(dataframe)
+    numeric = [smp_num for smp_num in numeric if smp_num != "PassengerId"]
+
+    for column in numeric:
+        _low, _up = outlier_thresholds(dataframe, column)
+        dataframe.loc[(dataframe[column] < _low), column] = _low
+        dataframe.loc[(dataframe[column] > _up), column] = _up
+
+
+data_frame = load_titanic()
+
+(categoric, numeric,
+ cat_in_numeric, cardinal_in_cat) = grab_col_names(data_frame)
+numeric = [smp_num for smp_num in numeric if smp_num != "PassengerId"]
+
+for column in numeric:
+    print(f"{column}: {check_outlier(data_frame, column)}")
+
+replace_with_thresholds(data_frame)
